4a7c68374e2070486641199755dfe8f2932e99f6
lfberge <lfberge@gmail.com>
2024-11-27 21:05:37 +0100

    slides

--- src/lib/insertSlide.ts
+++ src/lib/insertSlide.ts
@@ -1,6 +1,9 @@
+import { connection } from "next/server";
+import { SlideContent } from "./getSlide";
 import { getKVs } from "./kv";
 
-export async function insertSlide(key: string, data: any) {
+export async function insertSlide(key: string, data: SlideContent) {
+  await connection();
   const kv = await getKVs(["slides"]);
   await kv.slides.put(key, JSON.stringify(data));
 
