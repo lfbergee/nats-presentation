8cd32768c6ab2d455014e8f3be238a973a3838ac
Leiv Fredrik Berge <leiv.fredrik.berge@mattilsynet.no>
2025-02-11 08:38:05 +0100

    fix: update slides

--- src/app/insert/page.tsx
+++ src/app/insert/page.tsx
@@ -8,11 +8,16 @@ export default function Page({}) {
 
   const handleClick = () => {
     startTransition(async () => {
-      await insertSlide("slide.1", {
-        type: "interactive",
-        question: "Skal vi snakke mer om NATS?",
-        options: ["Ja", "Nei"],
-        notes: `Hello`,
+      await insertSlide("slide.18", {
+        type: "image",
+        image: "/procrast.jpg",
+        notes: `
+        Som en livslang og tildels professjonell
+        prokrastinerer. Så går dette inn i boka
+        som nok et eksempel på at prokastinering
+        lønner seg. Det er kanskje ikke lærdommen
+        fortjener, men det er den dere får.
+        `,
       });
     });
   };
